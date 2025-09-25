b70bb56bfa4dd835eb29ee21702b0955dceaa0f1
SRIKANTH A <147837484+srikaanthh@users.noreply.github.com>
2025-09-25 12:08:43 -0400

    Add files via upload

---
+++ public/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        usf: {
+          green: '#006747',
+          dark: '#004d35',
+          darker: '#003322',
+          light: '#e6f2ee'
+        },
+      },
+    },
+  },
+};
